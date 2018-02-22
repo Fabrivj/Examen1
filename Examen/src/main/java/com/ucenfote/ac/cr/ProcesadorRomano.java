47b12edcb63ed4a6a41ceb54957f222ea9458534
Fabricio Vargas <fvargasj@ucenfotec.ac.cr>
2018-02-21 23:14:22 -0600

    update
    Punto 1

--- Examen/src/main/java/com/ucenfote/ac/cr/ProcesadorRomano.java
+++ Examen/src/main/java/com/ucenfote/ac/cr/ProcesadorRomano.java
@@ -1,5 +1,14 @@
 package com.ucenfote.ac.cr;
 
+import java.util.HashMap;
+
 public class ProcesadorRomano {
+
+    public static int convierte(String valor) {
+
+        int result = 0;
+
+      return result;
+    }
 
 }
--- Examen/src/test/java/com/ucenfotec/ac/cr/TestProcesadorRomano.java
+++ Examen/src/test/java/com/ucenfotec/ac/cr/TestProcesadorRomano.java
@@ -1,14 +1,15 @@
 package com.ucenfotec.ac.cr;
 
+import com.ucenfote.ac.cr.ProcesadorRomano;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class TestProcesadorRomano {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void retorna0SiRecibeStringVacio() {
+        assertEquals(ProcesadorRomano.convierte(""), 0);
+    }
 
 }
